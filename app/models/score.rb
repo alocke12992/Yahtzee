cce7aedd78f4bc3150a5482fd1c849eb39998f4c
Andrew Locke <alocke12992@gmail.com>
2018-03-29 10:04:21 -0600

    Fixes scores model

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,10 +1,10 @@
 class Score < ApplicationRecord
   belongs_to :user
 
-  #Score.all_scores 
+  #Score.all_scores
   def self.all_scores
-    select('Value, user_id, u.email, scores.id')
-      joins('INNER JOIN users u ON u.id = sscores.user_id')
-      .order(value: :desc)
-  end 
+    select('value, user_id, u.email, scores.id')
+     joins('INNER JOIN users u ON u.id = scores.user_id')
+    .order(value: :desc)
+  end
 end
